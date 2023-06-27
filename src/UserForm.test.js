3ac663003c1be26d23971a467452ba4d64fd7388
michaelroesinger <mroesinger@gmail.com>
2023-06-27 12:50:04 -0700

    practice

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -12,3 +12,35 @@ test("it shows and a button", () => {
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
 });
+
+test("it calls onUserAdd when the form is submitted", () => {
+  //not best implementation
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+
+  //try to render my component
+  render(<UserForm onUserAdd={callback} />);
+
+  //Find the two inputs
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+  //simulate typing in a name
+  user.click(nameInput);
+  user.keyboard("jane");
+
+  //simulate typing in an email
+  user.click(emailInput);
+  user.keyboard("jane@jane.com");
+
+  //find the button
+  const button = screen.getByRole("button");
+
+  //simulate clicking the button
+  user.click(button);
+
+  //assertion to make sure "onUserAdd" gets called with email/name
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: "jane", email: "jane@jane.com" });
+});
